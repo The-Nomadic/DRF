c095df789e65ecb3efe809c6cbe19d9c7a211ed9
Rarij R <rarijr5144@gmail.com>
2024-01-08 22:28:56 +0530

    Importing data from Django to Excel DB

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='Index'),
+    path('import-data/', views.import_students_from_excel, name='Import-Data'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import openpyxl
+from .models import Student
 
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to my API Backend")
+
+# Import Data
+def import_students_from_excel(request):
+
+    file_path = "/Programs/REST APIs/DRF/backend/students_data.xlsx"
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+
+    # Get the fields of the Student model
+    model_fields = [field.name for field in Student._meta.get_fields() if field.name != "id"]
+
+    # Iterate over rows and create Student objects
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        student_data = dict(zip(model_fields, row))
+        # print(student_data)
+        Student.objects.create(**student_data)
+
+    return HttpResponse("Import completed")
